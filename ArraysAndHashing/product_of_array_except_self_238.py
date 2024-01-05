1d85fb28579eb30d22c3aa0747b69f979f2d2ae7
Zumh <17211423+Zumh@users.noreply.github.com>
2024-01-04 19:27:24 -0500

    product of array except self 238

--- ArraysAndHashing/product_of_array_except_self_238.py
+++ ArraysAndHashing/product_of_array_except_self_238.py
@@ -11,9 +11,37 @@ Example 1:
 
 Input: nums = [1,2,3,4]
 Output: [24,12,8,6]
+
 Example 2:
 
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 """
 
+# O(n) time complexity
+nums = [1,2,3,4]
+
+result = []
+product  = 1
+# multiplying from left to right
+for i in range(len(nums)):
+    result.append(product)
+
+    product *= nums[i]  
+
+
+
+# reset product
+product = 1
+print(nums)
+print(result)
+# multiplying from right to left with result array from previous calculation
+for i in range(len(nums)-1, -1, -1):
+    # 6 * 1 = 6 ....
+ 
+    result[i] *= product
+    product *= nums[i]
+
+print(result)
+
+    
